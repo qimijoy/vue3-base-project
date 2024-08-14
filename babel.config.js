c175d1e7b94ab4cd1534112e519560554823c437
Michael Panov <qimijoy@yandex.ru>
2024-08-14 15:52:14 +0300

    update vite & babel configs

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,7 @@ export default function primaryConfig(api) {
 				corejs: '3.38',
 			},
 		],
+		'@babel/preset-typescript',
 	];
 
 	const plugins = [];
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), babel()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
